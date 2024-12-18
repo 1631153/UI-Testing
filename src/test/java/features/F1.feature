d71ce5d10ed532a644a48994ea42f1df35c8b3be
Jacotet <cuentafree2003@gmail.com>
2024-12-18 19:42:28 +0100

    Escenario 1 (A medias)
    Se me había olvidado que esto era TQS y lo estaba haciendo mientras arreglaba el codigo xd

--- src/test/java/features/F1.feature
+++ src/test/java/features/F1.feature
@@ -1,4 +1,15 @@
-Feature: Navegar a la página principal
-Scenario: Abrir la página principal
-    
+Feature: Registro de usuario
+Scenario: Registro exitoso de un nuevo usuario
+
 Given the user is in the homepage
+When the user clicks to go to the registration page
+And the user fills in the registration form with the following details:
+    | First Name   | Javi          |
+    | Last Name    | David           |
+    | Email        | javi.david@test.com |
+    | Telephone    | 123456789     |
+    | Password     | Password123   |
+    | Confirm      | Password123   |
+And the user agrees to the privacy policy
+And the user clicks on the continue button
+Then the user should see a confirmation message
--- src/test/java/steps/Steps_Scenario1.java
+++ src/test/java/steps/Steps_Scenario1.java
@@ -5,16 +5,52 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+import java.util.Map;
 
 public class Steps_Scenario1 {
     
     WebDriver driver;
     
     @Given("the user is in the homepage")
-    public void TheUserIsInTheHomepage() {
-        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.navigate().to("https://opencart.abstracta.us/index.php?route=common/home");
-        driver.findElement(By.className("fc-button-label")).click();
+	public void TheUserIsInTheIndexPage() {
+		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("https://opencart.abstracta.us/index.php?route=common/home");
+	} 
+    
+    @When("the user clicks to go to the registration page")
+    public void TheUserClicksToGoToTheRegistrationPage() {
+    	driver.findElement(By.cssSelector("a[title='My Account']")).click();
+        driver.findElement(By.linkText("Register")).click();
+    }
+    
+    @When("the user fills in the registration form with the following details:")
+    public void theUserFillsInTheRegistrationFormWithTheFollowingDetails(Map<String, String> userDetails) {
+    	driver.findElement(By.id("input-firstname")).sendKeys(userDetails.get("First Name"));
+        driver.findElement(By.id("input-lastname")).sendKeys(userDetails.get("Last Name"));
+        driver.findElement(By.id("input-email")).sendKeys(userDetails.get("Email"));
+        driver.findElement(By.id("input-telephone")).sendKeys(userDetails.get("Telephone"));
+        driver.findElement(By.id("input-password")).sendKeys(userDetails.get("Password"));
+        driver.findElement(By.id("input-confirm")).sendKeys(userDetails.get("Confirm"));
+    }
+    
+    @When("the user agrees to the privacy policy")
+    public void TheUserAgreesToThePrivacyPolicy() {
+    	driver.findElement(By.name("agree")).click();
+    }
+    
+    @When("the user clicks on the continue button")
+    public void TheUserClicksOnTheContinueButton() {
+    	driver.findElement(By.className("btn btn-primary")).click();
+    }
+    
+    @Then("the user should see a confirmation message")
+    public void theUserShouldSeeAConfirmationMessage() {
+        String expectedMessage = "Congratulations! Your new account has been successfully created!";
+        String actualMessage = driver.findElement(By.cssSelector("div#content p")).getText();
+        Assert.assertEquals(actualMessage, expectedMessage, "The success message is incorrect.");
     }
 }
