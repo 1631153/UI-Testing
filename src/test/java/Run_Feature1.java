4ac58b280955bc4e3be5c864d4299f4cfc148d4c
Setset19 <davidbonillamedina@gmail.com>
2024-12-29 14:51:44 +0100

    Borro los featrues 1 y 2
    Por temas de compatibilidad con le trabajo de mi compañero he borrado los features 1 y 2 porque no son mios, solo los use de ejemplo para los siguientes features y por lo tanto no deberia de tenerlos en mi rama.

--- src/test/java/Run_Feature1.java
+++
@@ -1,7 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/F1.feature", glue="steps")
-public class Run_Feature1 extends AbstractTestNGCucumberTests {
-
-}
--- src/test/java/Run_Feature2.java
+++
@@ -1,7 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/F2.feature", glue="steps")
-public class Run_Feature2 extends AbstractTestNGCucumberTests {
-
-}
